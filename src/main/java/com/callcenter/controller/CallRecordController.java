9d15d34cedc5b2ed8e5eea6f6ce31b1815a640f1
Deep Shah <dshah@spiderlogic.com>
2010-10-03 12:58:26 +0530

    CC-05| Adding the fields for the search form <Deep>

--- src/main/java/com/callcenter/controller/CallRecordController.java
+++ src/main/java/com/callcenter/controller/CallRecordController.java
@@ -1,11 +1,10 @@
 package com.callcenter.controller;
 
-import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
 import com.callcenter.domain.CallRecord;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -23,14 +22,17 @@ public class CallRecordController {
     @RequestMapping(value = "/callrecord", method = RequestMethod.GET)
     public String list(@RequestParam(value = "page", required = false) Integer page,
                        @RequestParam(value = "size", required = false) Integer size, ModelMap modelMap) {
-        if (page != null || size != null) {
-            int sizeNo = size == null ? 10 : size.intValue();
-            modelMap.addAttribute("callrecords", CallRecord.findCallRecordEntries(page == null ? 0 : (page.intValue() - 1) * sizeNo, sizeNo));
-            float nrOfPages = (float) CallRecord.countCallRecords() / sizeNo;
-            modelMap.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
-        } else {
+        if (page == null && size == null) {
             modelMap.addAttribute("callrecords", CallRecord.findAllCallRecords());
+            return "callrecord/list";
         }
+
+        int sizeNo = size == null ? 10 : size.intValue();
+        final int pageNo = page == null ? 0 : (page.intValue() - 1) * sizeNo;
+        modelMap.addAttribute("callrecords", CallRecord.findCallRecordEntries(pageNo, sizeNo));
+
+        float nrOfPages = (float) CallRecord.countCallRecords() / sizeNo;
+        modelMap.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
         return "callrecord/list";
     }
 
