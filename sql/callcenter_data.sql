148c30e55db22dd23b6e7712fa22d7f9ba507efc
Deep Shah <dshah@spiderlogic.com>
2010-11-15 19:02:37 +0530

    CC-08| The callcenter data <Deep>

--- sql/callcenter_data.sql
+++ sql/callcenter_data.sql
@@ -59,6 +59,170 @@ INSERT INTO `call_record` VALUES (1,0,NULL,'2002','Extn2002','2001','Extn2001','
 UNLOCK TABLES;
 
 --
+-- Table structure for table `fields`
+--
+
+DROP TABLE IF EXISTS `fields`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fields` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` int(11) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fields`
+--
+
+LOCK TABLES `fields` WRITE;
+/*!40000 ALTER TABLE `fields` DISABLE KEYS */;
+INSERT INTO `fields` VALUES (1,1,'SessionCall Id','callerId'),(2,1,'Calling Party Name','callingPartyName'),(3,1,'Called Party Name','calledPartyName');
+/*!40000 ALTER TABLE `fields` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `group_members`
+--
+
+DROP TABLE IF EXISTS `group_members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `group_members` (
+  `group_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`group_id`,`user_id`),
+  KEY `FK3B9C77591C637EB6` (`user_id`),
+  KEY `FK3B9C77599D5263FE` (`group_id`),
+  CONSTRAINT `FK3B9C77591C637EB6` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `FK3B9C77599D5263FE` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `group_members`
+--
+
+LOCK TABLES `group_members` WRITE;
+/*!40000 ALTER TABLE `group_members` DISABLE KEYS */;
+INSERT INTO `group_members` VALUES (1,1),(2,1),(3,1),(4,1);
+/*!40000 ALTER TABLE `group_members` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `groups`
+--
+
+DROP TABLE IF EXISTS `groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `groups` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `number` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `groups`
+--
+
+LOCK TABLES `groups` WRITE;
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+INSERT INTO `groups` VALUES (1,0,'testinggroup','12234'),(2,0,'Administrators','+9165765656'),(3,0,'kjkj','90909'),(4,0,'CreditCard Agents','3000');
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `restriction_values`
+--
+
+DROP TABLE IF EXISTS `restriction_values`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `restriction_values` (
+  `restriction_id` bigint(20) NOT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  `value_index` int(11) NOT NULL,
+  PRIMARY KEY (`restriction_id`,`value_index`),
+  KEY `FKD0A5E8F5D4B1B9DE` (`restriction_id`),
+  CONSTRAINT `FKD0A5E8F5D4B1B9DE` FOREIGN KEY (`restriction_id`) REFERENCES `restrictions` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `restriction_values`
+--
+
+LOCK TABLES `restriction_values` WRITE;
+/*!40000 ALTER TABLE `restriction_values` DISABLE KEYS */;
+/*!40000 ALTER TABLE `restriction_values` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `restrictions`
+--
+
+DROP TABLE IF EXISTS `restrictions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `restrictions` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` int(11) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `field` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKBB8E66277738BAD6` (`role_id`),
+  KEY `FKBB8E662784E03898` (`field`),
+  CONSTRAINT `FKBB8E66277738BAD6` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
+  CONSTRAINT `FKBB8E662784E03898` FOREIGN KEY (`field`) REFERENCES `fields` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `restrictions`
+--
+
+LOCK TABLES `restrictions` WRITE;
+/*!40000 ALTER TABLE `restrictions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `restrictions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role_allowed_fields`
+--
+
+DROP TABLE IF EXISTS `role_allowed_fields`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role_allowed_fields` (
+  `role_id` bigint(20) NOT NULL,
+  `field_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`role_id`,`field_id`),
+  KEY `FK28BD00197738BAD6` (`role_id`),
+  KEY `FK28BD001947B20E1E` (`field_id`),
+  CONSTRAINT `FK28BD001947B20E1E` FOREIGN KEY (`field_id`) REFERENCES `fields` (`id`),
+  CONSTRAINT `FK28BD00197738BAD6` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role_allowed_fields`
+--
+
+LOCK TABLES `role_allowed_fields` WRITE;
+/*!40000 ALTER TABLE `role_allowed_fields` DISABLE KEYS */;
+INSERT INTO `role_allowed_fields` VALUES (3,1),(3,2),(4,1),(4,3),(5,2),(5,3);
+/*!40000 ALTER TABLE `role_allowed_fields` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `roles`
 --
 
@@ -72,7 +236,7 @@ CREATE TABLE `roles` (
   `language` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -81,7 +245,7 @@ CREATE TABLE `roles` (
 
 LOCK TABLES `roles` WRITE;
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,1,'Admin User',NULL,'ROLE_ADMIN');
+INSERT INTO `roles` VALUES (1,1,'Administrators',NULL,'ROLE_ADMIN'),(2,1,'Default',NULL,'ROLE_DEFAULT'),(3,1,'Basic',NULL,'ROLE_BASIC');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -125,4 +289,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-10-26 17:16:31
+-- Dump completed on 2010-11-15 17:54:59
