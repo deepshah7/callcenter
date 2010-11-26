660498828b3b19c57d44d875b46f8f628a7f2084
Deep Shah <dshah@spiderlogic.com>
2010-11-26 10:14:13 +0530

    CC-06| Changing the urls of all the recording library services <Deep>

--- src/main/java/com/callcenter/controller/RecordingLibraryServiceController.java
+++ src/main/java/com/callcenter/controller/RecordingLibraryServiceController.java
@@ -3,7 +3,6 @@ package com.callcenter.controller;
 import com.callcenter.domain.RecordingLibraryService;
 import com.callcenter.domain.Service;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
@@ -14,42 +13,43 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class RecordingLibraryServiceController {
 
     @RequestMapping(value = "/recordinglibrary", method = RequestMethod.POST)
-    public String create(final Service service, final ModelMap modelMap) {
+    public String create(final Service service) {
         service.persist();
-        return "redirect:/service/" + service.getId();
+        return "redirect:/recordinglibrary/" + service.getId();
     }
 
     @RequestMapping(value = "/recordinglibrary/form", method = RequestMethod.GET)
     public String createForm(ModelMap modelMap) {
         modelMap.addAttribute("service", new RecordingLibraryService());
-        return "service/create";
+        return "recordinglibrary/create";
     }
 
-    @RequestMapping(value = "/service/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/recordinglibrary/{id}", method = RequestMethod.GET)
     public String show(@PathVariable("id") Long id, ModelMap modelMap) {
-        if (id == null) throw new IllegalArgumentException("An Identifier is required");
-        modelMap.addAttribute("service", Service.findService(id));
-        return "service/show";
+        modelMap.addAttribute("service", RecordingLibraryService.findRecordingLibraryService(id));
+        return "recordinglibrary/show";
     }
 
-    @RequestMapping(value = "/service", method = RequestMethod.GET)
-    public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, ModelMap modelMap) {
+    @RequestMapping(value = "/recordinglibrary", method = RequestMethod.GET)
+    public String list(@RequestParam(value = "page", required = false) Integer page,
+                       @RequestParam(value = "size", required = false) Integer size, ModelMap modelMap) {
         if (page != null || size != null) {
             int sizeNo = size == null ? 10 : size.intValue();
-            modelMap.addAttribute("services", Service.findServiceEntries(page == null ? 0 : (page.intValue() - 1) * sizeNo, sizeNo));
-            float nrOfPages = (float) Service.countServices() / sizeNo;
+            modelMap.addAttribute("services", RecordingLibraryService.findRecordingLibraryServiceEntries(page == null ? 0 : (page.intValue() - 1) * sizeNo, sizeNo));
+            float nrOfPages = (float) RecordingLibraryService.countRecordingLibraryServices() / sizeNo;
             modelMap.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
         } else {
-            modelMap.addAttribute("services", Service.findAllServices());
+            modelMap.addAttribute("services", RecordingLibraryService.findAllServices());
         }
-        return "service/list";
+        return "recordinglibrary/list";
     }
 
-    @RequestMapping(value = "/service/{id}", method = RequestMethod.DELETE)
-    public String delete(@PathVariable("id") Long id, @RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size) {
+    @RequestMapping(value = "/recordinglibrary/{id}", method = RequestMethod.DELETE)
+    public String delete(@PathVariable("id") Long id, @RequestParam(value = "page", required = false) Integer page,
+                         @RequestParam(value = "size", required = false) Integer size) {
         if (id == null) throw new IllegalArgumentException("An Identifier is required");
-        Service.findService(id).remove();
-        return "redirect:/service?page=" + ((page == null) ? "1" : page.toString()) + "&size=" + ((size == null) ? "10" : size.toString());
+        RecordingLibraryService.findRecordingLibraryService(id).remove();
+        return "redirect:/recordinglibrary?page=" + ((page == null) ? "1" : page.toString()) + "&size=" + ((size == null) ? "10" : size.toString());
     }
 
 }
